d683e090e1a8d9408eb2cd33bcdfea5b97d1d0f9
Tal Yitzhak <taly@lightrun.com>
2025-02-07 21:23:20 +0200

    latest changes

--- amplify/backend/storage/slgfiles/override.ts
+++
@@ -1,7 +0,0 @@
-import { AmplifyS3ResourceTemplate } from '@aws-amplify/cli-extensibility-helper';
-
-export function override(resources: AmplifyS3ResourceTemplate) {
-    resources.s3Bucket.accelerateConfiguration = {
-        accelerationStatus: 'Enabled'
-    }
-}
