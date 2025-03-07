56ef464628dbc3f6715a2b34162809ca4c8d00de
Tal Yitzhak <taly@lightrun.com>
2025-03-07 12:31:49 +0200

    added assignment references to the project

--- start.sh
+++ start.sh
@@ -4,8 +4,5 @@ amplify add auth
 amplify push
 amplify hosting add
 amplify add predictions
-#"identifyDoc": true,
-#"access": "auth | authAndGuest",
-#"format": "PLAIN | FORM | TABLE | ALL"
 amplify publish
 amplify status
